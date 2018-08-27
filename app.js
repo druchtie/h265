d3fa610d3d63f57b83907a509a0a12b0c65df1f0
Danny Ruchtie <druchtie@gmail.com>
2018-08-27 07:51:37 +0200

    onclick now working
    onclick now working

--- app.js
+++ app.js
@@ -4,6 +4,36 @@ var command = ffmpeg();
 
 
 
+function convert(){
+alert('1');	
+	
+ffmpeg(f.path)
+		
+		  .videoCodec('libx265')	
+		  .size('?x1080')
+		  .videoBitrate(400000)
+		  .outputOptions(['-strict -2' ,'-tag:v hvc1', '-preset medium', '-profile:v main10'])
+		  .output( dir+'-converted.mp4')
+	
+
+.on('progress', function(progress) {
+	var percent = Math.round(progress.percent);
+
+
+	})
+	
+  .on('end', function() {
+  	event.dataTransfer.dropEffect = 'link';
+  })
+  
+.run();
+  
+alert('1');	
+};
+
+document.getElementById("cbt").addEventListener('click', convert);
+
+
 
 
 
@@ -19,40 +49,15 @@ document.addEventListener('drop', function (e) {
 	document.getElementById("video").innerHTML= '<video controls><source src="'+path+'"></video>';	
 	event.dataTransfer.dropEffect = 'none';
 
-/*
-		ffmpeg(f.path)
-		
-		  .videoCodec('libx265')	
-		  .size('?x1080')
-		  .videoBitrate(400000)
-		  .outputOptions(['-strict -2' ,'-tag:v hvc1', '-preset medium', '-profile:v main10'])
-		  .output( dir+'-converted.mp4')
-	
-	
-		  
-
-		  
-
-.on('progress', function(progress) {
-	var percent = Math.round(progress.percent);
-
-
-	})
-	
-  .on('end', function() {
-  	event.dataTransfer.dropEffect = 'link';
-  })
   
-  .run();
-*/   
       }
-    });
+});
     
     
-    document.addEventListener('dragover', function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-    });
+document.addEventListener('dragover', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+});
 
 
 
